b58a1a71452e9d44c3adb67038c21682d82bd1a0
chliyang <chengli.yang@thoughtworks.com>
2022-04-19 22:36:27 +0800

    [useEffect] practice using useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import React, { useState } from "react";
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [inputValue, setInputValue] = useState("");
+  const [showHint, setShowHint] = useState(false);
 
   const handleChange = (event) => {
     setInputValue(event.target.value);
   };
+
+  useEffect(() => {
+    if (inputValue.length > 8) setShowHint(true);
+    if (inputValue.length <= 8) setShowHint(false);
+  }, [inputValue]);
 
   return (
     <>
@@ -18,7 +23,11 @@ function App() {
       </div>
       <div>
         <input type="text" onChange={handleChange} />
-        <p>input value is {inputValue}</p>
+        {showHint ? (
+          <p>sorry, input length cannot greater than 8.</p>
+        ) : (
+          <p>input value is {inputValue}</p>
+        )}
       </div>
     </>
   );
