30810bcccbb0bbe4640c86a86d31ff3c5be69b11
chliyang <chengli.yang@thoughtworks.com>
2022-04-22 02:04:14 +0800

    [tic-tac-toe] finish layout & drop chess & reset

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import "./App.css";
-import React, { useContext, useReducer } from "react";
-import useApp from "./hooks/use-app";
-import { ThemeContext } from "./context/theme-context";
-import countReducer from "./reducer/count-reducer";
+import React from "react";
+// import React, { useContext, useReducer } from "react";
+// import useApp from "./hooks/use-app";
+// import { ThemeContext } from "./context/theme-context";
+// import countReducer from "./reducer/count-reducer";
+import TicTacToe from "./components/tic-tac-toe";
 
-const initialState = { count: 0 };
+// const initialState = { count: 0 };
 
 function App() {
-  const theme = useContext(ThemeContext);
-  const [state, dispatch] = useReducer(countReducer, initialState);
+  // const theme = useContext(ThemeContext);
+  // const [state, dispatch] = useReducer(countReducer, initialState);
 
-  const {
-    count,
-    setCount,
-    showHint,
-    inputValue,
-    inputRef,
-    handleChange,
-    focusOn,
-  } = useApp();
+  // const {
+  //   count,
+  //   setCount,
+  //   showHint,
+  //   inputValue,
+  //   inputRef,
+  //   handleChange,
+  //   focusOn,
+  // } = useApp();
 
   return (
-    <>
-      <div>
+    <div className="container">
+      <TicTacToe />
+      {/* <div>
         <p>You clicked {count} times</p>
         <button
-          style={{ background: theme.background, color: theme.foreground }}
+          style={{
+            background: theme.background,
+            color: theme.foreground,
+            marginBottom: "40px",
+          }}
           onClick={() => setCount(count + 1)}
         >
           Click me
@@ -45,8 +52,8 @@ function App() {
         <p>You clicked {state.count} times</p>
         <button onClick={() => dispatch({ type: "decrement" })}>-</button>
         <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      </div>
-    </>
+      </div> */}
+    </div>
   );
 }
 
---
+++ src/components/tic-tac-toe.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const initialChessState = ["", "", "", "", "", "", "", "", ""];
+
+const TicTacToe = () => {
+  const [currentChess, setCurrentChess] = useState("O");
+  const [chessState, setChessState] = useState(initialChessState);
+
+  const handleClick = (index) => {
+    const xORo = currentChess === "X" ? "O" : "X";
+    setCurrentChess(xORo);
+    setChessState((preValue) => {
+      const arr = [...preValue];
+      arr[index] = xORo;
+      return arr;
+    });
+  };
+
+  const resetChess = () => {
+    setChessState(initialChessState);
+  };
+
+  const renderChessBoard = () => {
+    const result = [];
+    for (let i = 0; i < 9; i++) {
+      result.push(
+        <div onClick={() => handleClick(i)} key={i} className="square">
+          {chessState[i]}
+        </div>
+      );
+    }
+    return <section className="chessboard">{result}</section>;
+  };
+
+  return (
+    <main className="center">
+      <section className="next_one">
+        Next round: {currentChess === "X" ? "O" : "X"}
+      </section>
+      <section className="judgement">Playing^_^</section>
+      {renderChessBoard()}
+      <button onClick={resetChess}>Reset</button>
+    </main>
+  );
+};
+
+export default TicTacToe;
