5e26ec6cabbb7d15259c43ee5576d86d43015f08
chliyang <chengli.yang@thoughtworks.com>
2022-04-29 17:04:40 +0800

    fix: end game as a tie

--- src/components/hooks/use-tic-tac-toe.js
+++ src/components/hooks/use-tic-tac-toe.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import adjudication from "../../utils/adjudication";
-import { CHESS_X, CHESS_O } from "../../constant/chess-constant";
+import { CHESS_X, CHESS_O, WIN_MESSAGE_TIE } from "../../constant/chess-constant";
 
 const initialChessState = ["", "", "", "", "", "", "", "", ""];
 
@@ -15,10 +15,12 @@ const useTicTacToe = () => {
   }, [chessState]);
 
   useEffect(() => {
-    if (winner !== undefined)
+    if (winner !== undefined && winner !== WIN_MESSAGE_TIE){
       currentChess === CHESS_X
-        ? setRecord((preValue) => ({ ...record, X: preValue.X + 1 }))
-        : setRecord((preValue) => ({ ...record, O: preValue.O + 1 }));
+      ? setRecord((preValue) => ({ ...record, X: preValue.X + 1 }))
+      : setRecord((preValue) => ({ ...record, O: preValue.O + 1 }));
+    }
+     
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [winner]);
 
